32743d26491bc0d6fcc901775c78879986672228
VitalijVictor <vitalijvictor@gmail.com>
2022-05-13 09:31:42 +0200

    five config

--- src/test/Main.java
+++ src/test/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("2 config");
         System.out.println("3 config");
         System.out.println("4 config");
+        System.out.println("5 config");
     }
 }
