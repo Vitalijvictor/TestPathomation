f8f54528e7dc5c46aa1ffdc43508c7318f2b0696
VitalijVictor <vitalijvictor@gmail.com>
2022-05-13 09:15:43 +0200

    Second config

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Check for Jenkins");
         System.out.println("1 config");
+        System.out.println("2 config");
     }
 }
