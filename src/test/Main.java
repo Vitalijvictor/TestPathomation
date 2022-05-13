224d7f8d64d20a906823f8a34e876e9e15e156f9
VitalijVictor <vitalijvictor@gmail.com>
2022-05-13 09:12:25 +0200

    First config

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Check for Jenkins");
+        System.out.println("1 config");
     }
 }
